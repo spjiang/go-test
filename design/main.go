1755c7a695daed753140a249999676b71b2542eb
spjiang <spjiang@aliyun.com>
2021-12-02 16:13:42 +0800

    test

--- design/main.go
+++ design/main.go
@@ -1,12 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/spjiang/go-test/design/facade"
-)
-
 func main() {
-	a := facade.NewAPI()
-	s := a.Test()
-	fmt.Println(s)
+
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/spjiang/go-test
 
-go 1.15
+go 1.16
 
 require (
 	github.com/go-resty/resty/v2 v2.4.0
